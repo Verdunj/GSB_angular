af71ef08d3237478899b27f7c3b8c2fbb77a2c3f
Verdunj <joris42350@yahoo.fr>
2018-06-05 10:12:27 +0200

    ajout du composant medicament dans le fichier app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,33 +1,31 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from '@angular/forms';
-import {RouterModule, Routes} from '@angular/router';
-import {HttpModule} from '@angular/http';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
 
-import {ConnexionComponent} from './connexion/app.connexion.component';
-import {MedecinsComponent} from './medecins/app.medecins.component';
-import {VisitesComponent} from './visites/app.visites.component';
-import {NavbarComponent} from './navbar/app.navbar.component';
-import {AppComponent} from './app.component';
-import {DataService} from './service/app.service.data';
-import {Sha1Service} from './service/app.service.crypto';
+import { AppComponent }   from './app.component';
+import { ConnexionComponent }   from './connexion/app.connexion.component';
+import { MedecinsComponent }  from './medecins/app.medecins.component';
+import { VisitesComponent }  from './visites/app.visites.component';
+import { NavbarComponent }  from './navbar/app.navbar.component';
+import { MedicamentComponent } from './medicament/app.medicament.component';
+import {DataService} from './services/app.service.data';
+import { Sha1Service} from './services/app.service.crypto';
 
 
 const appRoutes: Routes = [
-    {path: '', component: ConnexionComponent},
-    {path: 'connexion', component: ConnexionComponent},
-    {path: 'medecins', component: MedecinsComponent},
-    {path: 'visites', component: VisitesComponent},
-    {path: 'accueil', component: NavbarComponent}
+  { path: '', component: ConnexionComponent },
+  { path: 'medecins', component: MedecinsComponent },
+  { path: 'visites', component: VisitesComponent },
+  { path: 'accueil',  component: NavbarComponent  }
 ];
 
 
 @NgModule({
-    imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes), HttpModule],
-    declarations: [AppComponent, ConnexionComponent, NavbarComponent, MedecinsComponent, VisitesComponent],
-    providers: [DataService, Sha1Service],
-    bootstrap: [AppComponent]
+  imports:      [ BrowserModule,FormsModule, HttpModule,RouterModule.forRoot(appRoutes) ],
+  declarations: [ AppComponent,ConnexionComponent,MedecinsComponent, VisitesComponent,NavbarComponent, ],
+  providers : [DataService,Sha1Service],
+  bootstrap:    [ AppComponent ]
 })
-
-
-export class AppModule {}
+export class AppModule { }

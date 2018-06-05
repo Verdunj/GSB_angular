5bfc1e9bcb63c94658a3edb6f1c630dd12313ec9
Verdunj <joris42350@yahoo.fr>
2018-06-05 10:04:42 +0200

    Ajout medicament

---
+++ app/medicament/app.medicament.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService} from '../services/app.service.data';
+import{ Observable } from 'rxjs/Rx';
+
+@Component({
+  moduleId: module.id,
+  selector: 'my-medicament',
+  templateUrl: 'app.medicament.html'
+
+})
